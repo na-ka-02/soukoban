b087f5e3109566d0f468bc8ab06925aa59be00d0
na <k023g1066@g.neec.ac.jp>
2024-05-30 17:16:06 +0900

    倉庫番つくった

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+
 public class GameManagerScript : MonoBehaviour
 {
+    public CinemachineVirtualCameraBase virtualCamera;//カメラポジションをプレイヤー中心にする
     public GameObject playerPrefab;
     public GameObject boxPrefab;
     /// <summary>荷物を格納する場所のプレハブ</summary>
     public GameObject storePrefab;
     /// <summary>クリアーしたことを示すテキストの GameObject</summary>
     public GameObject clearText;
+    public GameObject wallPrefab;
     int[,] map; // マップの元データ（数字）
     GameObject[,] field;    // map を元にしたオブジェクトの格納庫
 
@@ -49,9 +53,9 @@ public class GameManagerScript : MonoBehaviour
     bool MoveNumber(Vector2Int moveFrom, Vector2Int moveTo)
     {
         // 動けない場合は false を返す
-        if (moveTo.y < 0 || moveTo.y >= field.GetLength(0))
+        if (moveTo.y < 1 || moveTo.y >= field.GetLength(0) - 1)
             return false;
-        if (moveTo.x < 0 || moveTo.x >= field.GetLength(1))
+        if (moveTo.x < 1 || moveTo.x >= field.GetLength(1) - 1)
             return false;
         if (field[moveTo.y, moveTo.x] != null
             && field[moveTo.y, moveTo.x].tag == "Box")
@@ -104,12 +108,15 @@ public class GameManagerScript : MonoBehaviour
 
         map = new int[,]
         {
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 3, 2, 2, 1, 0, 0, 0, 0, 0 },
-            { 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 3 },
-            { 3, 0, 0, 0, 0, 3, 2, 2, 1, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0 },
-        };  // 0: 何もない, 1: プレイヤー, 2: 箱
+            { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
+            { 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 },
+            { 4, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 4 },
+            { 4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 4 },
+            { 4, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 4 },
+            { 4, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 4 },
+            { 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 },
+            { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
+        };  // 0: 何もない, 1: プレイヤー, 2: 箱,3:箱を格納する場所,4:壁
 
         field = new GameObject
         [
@@ -127,7 +134,8 @@ public class GameManagerScript : MonoBehaviour
                         new Vector3(x, -1 * y, 0),
                         Quaternion.identity);
                     field[y, x] = instance; // プレイヤーを保存しておく
-                    break;  // プレイヤーは１つだけなので抜ける
+                    virtualCamera.Follow = instance.transform;//カメラをプレイヤー追従にする
+                    //break;  // プレイヤーは１つだけなので抜ける
                     // break;  // プレイヤーは１つだけなので抜ける
                 }   // プレイヤーを出す
                 else if (map[y, x] == 2)
@@ -135,8 +143,8 @@ public class GameManagerScript : MonoBehaviour
                     GameObject instance =
                         Instantiate(boxPrefab,
                         new Vector3(x, -1 * y, 0),
-                        Quaternion.identity);
-                    field[y, x] = instance; // 箱を保存しておく
+                        Quaternion.identity); // 箱を保存しておく
+                    field[y, x] = instance;
                 }   // 箱を出す
                 else if (map[y, x] == 3)
                 {
@@ -144,20 +152,30 @@ public class GameManagerScript : MonoBehaviour
                         Instantiate(storePrefab,
                         new Vector3(x, -1 * y, 0),
                         Quaternion.identity);
+                    field[y, x] = instance;
                 }   // 格納場所を出す
+                else if (map[y, x] == 4)
+                {
+                    GameObject instance =
+                        Instantiate(wallPrefab,
+                        new Vector3(x, -1 * y, 0),
+                        Quaternion.identity);
+                    field[y, x] = instance;
+                }
             }
         }
     }
     void Update()
     {
+
+        if (IsClear())
+            clearText.SetActive(true);
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             var playerPosition = GetPlayerIndex();
             MoveNumber(playerPosition, new Vector2Int(playerPosition.x + 1, playerPosition.y));    // →に移動
 
-            if (IsClear())
-                Debug.Log("Clear");
-            clearText.SetActive(true);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -165,9 +183,9 @@ public class GameManagerScript : MonoBehaviour
             var playerPosition = GetPlayerIndex();
             MoveNumber(playerPosition, new Vector2Int(playerPosition.x - 1, playerPosition.y));    // ←に移動
 
-            if (IsClear())
-                Debug.Log("Clear");
-            clearText.SetActive(true);
+            //if (IsClear())
+            //    Debug.Log("Clear");
+            //clearText.SetActive(true);
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -175,9 +193,9 @@ public class GameManagerScript : MonoBehaviour
             var playerPosition = GetPlayerIndex();
             MoveNumber(playerPosition, new Vector2Int(playerPosition.x, playerPosition.y - 1));    // ↑に移動
 
-            if (IsClear())
-                Debug.Log("Clear");
-            clearText.SetActive(true);
+            //if (IsClear())
+            //    Debug.Log("Clear");
+            //clearText.SetActive(true);
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
@@ -185,9 +203,6 @@ public class GameManagerScript : MonoBehaviour
             var playerPosition = GetPlayerIndex();
             MoveNumber(playerPosition, new Vector2Int(playerPosition.x, playerPosition.y + 1));    // ↓に移動
 
-            if (IsClear())
-                Debug.Log("Clear");
-            clearText.SetActive(true);
         }
     }
 }
